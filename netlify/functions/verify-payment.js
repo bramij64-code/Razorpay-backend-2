e7aaf1174149b6771da2399a6a88b90c44d4d6f1
bramij64-code <bramij64@gmail.com>
2025-11-24 11:40:33 +0530

    Create verify-payment.js

---
+++ netlify/functions/verify-payment.js
@@ -0,0 +1,25 @@
+const crypto = require("crypto");
+
+exports.handler = async (event) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    JSON.parse(event.body);
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    .update(body.toString())
+    .digest("hex");
+
+  if (expectedSignature === razorpay_signature) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true }),
+    };
+  }
+
+  return {
+    statusCode: 400,
+    body: JSON.stringify({ success: false }),
+  };
+};
